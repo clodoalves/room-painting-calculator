3eca36db31b26b19ca4a1e6130d5e74c5945cb19
Jose Clodoalves <jrjcsj@gmail.com>
2024-03-19 22:12:20 -0300

    Implementing properties in Room class

--- room.py
+++ room.py
@@ -1,9 +1,29 @@
 class Room:
-    width:float
-    depth:float
-    heigth:float
+    __width:float
+    __depth:float
+    __heigth:float
 
     def __init__(self, width, depth):
         self.width = width
         self.depth = depth
-        self.heigth = 2.9
+        self.__heigth = 2.9
+
+    @property
+    def width(self):
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def depth(self):
+        return self.__depth    
+    
+    @depth.setter
+    def depth(self, value):
+        self.__depth = value
+
+    @property  
+    def heigth(self):
+        return self.__heigth
